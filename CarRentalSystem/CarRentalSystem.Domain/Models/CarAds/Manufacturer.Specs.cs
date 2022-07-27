7f56c292aada774a6f4c122d238de52d5bd448a8
Kiril Pecev <kir4o_pecev@abv.bg>
2022-07-27 22:50:44 +0300

    Added manufacturer unit tests

---
+++ CarRentalSystem/CarRentalSystem.Domain/Models/CarAds/Manufacturer.Specs.cs
@@ -0,0 +1,31 @@
+﻿namespace CarRentalSystem.Domain.Models.CarAds
+{
+    using Exceptions;
+
+    using FakeItEasy;
+    using FluentAssertions;
+    using Xunit;
+
+    public class ManufacturerSpecs
+    {
+        [Fact]
+        public void ValidManufacturerShouldNotThrowException()
+        {
+            // Act
+            Action act = () => A.Dummy<Manufacturer>();
+
+            // Assert
+            act.Should().NotThrow<InvalidCarAdException>();
+        }
+
+        [Fact]
+        public void InvalidNameShouldThrowException()
+        {
+            // Act
+            Action act = () => new Manufacturer("");
+
+            // Assert
+            act.Should().Throw<InvalidCarAdException>();
+        }
+    }
+}
