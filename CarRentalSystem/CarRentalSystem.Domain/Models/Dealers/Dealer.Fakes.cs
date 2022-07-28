9d6620faf924d384da2c451f06ede6aae74c867f
Kiril Pecev <kir4o_pecev@abv.bg>
2022-07-28 09:37:48 +0300

    Added phone number unit tests

---
+++ CarRentalSystem/CarRentalSystem.Domain/Models/Dealers/Dealer.Fakes.cs
@@ -0,0 +1,16 @@
+﻿namespace CarRentalSystem.Domain.Models.Dealers
+{
+    using FakeItEasy;
+
+    public class DealerFakes
+    {
+        public class DealerDummyFactory : IDummyFactory
+        {
+            public Priority Priority => Priority.Default;
+
+            public bool CanCreate(Type type) => type == typeof(Dealer);
+
+            public object? Create(Type type) => new Dealer("Valid dealer", A.Dummy<PhoneNumber>());
+        }
+    }
+}
---
+++ CarRentalSystem/CarRentalSystem.Domain/Models/Dealers/PhoneNumber.Fakes.cs
@@ -0,0 +1,18 @@
+﻿namespace CarRentalSystem.Domain.Models.Dealers
+{
+    using System;
+
+    using FakeItEasy;
+
+    public class PhoneNumberFakes
+    {
+        public class PhoneNumberDummyFactory : IDummyFactory
+        {
+            public Priority Priority => Priority.Default;
+
+            public bool CanCreate(Type type) => type == typeof(PhoneNumber);
+
+            public object? Create(Type type) => new PhoneNumber("+12345678");
+        }
+    }
+}
