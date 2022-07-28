ef0a015742d4c26a790c2c6171f0ee1a727c92cc
Kiril Pecev <kir4o_pecev@abv.bg>
2022-07-28 21:11:23 +0300

    Added Startup and Web projects

---
+++ CarRentalSystem/CarRentalSystem.Web/Features/CarAdsController.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRentalSystem.Web.Features
+{
+    using Domain.Models.CarAds;
+    using Domain.Models.Dealers;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CarAdsController : ControllerBase
+    {
+        private static readonly Dealer Dealer = new Dealer("Dealer", "+12345678");
+
+        [HttpGet]
+        public IEnumerable<CarAd> Get() => Dealer.CarAds.Where(c => c.IsAvailable);
+    }
+}
