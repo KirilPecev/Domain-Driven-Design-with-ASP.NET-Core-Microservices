4a939e0b3db9bd33a6a3ed0148e712f8cda67d85
Kiril Pecev <kir4o_pecev@abv.bg>
2022-07-28 21:20:49 +0300

    Added Swagger

--- CarRentalSystem/CarRentalSystem.Web/Features/CarAdsController.cs
+++ CarRentalSystem/CarRentalSystem.Web/Features/CarAdsController.cs
@@ -1,17 +1,12 @@
 ﻿namespace CarRentalSystem.Web.Features
 {
-    using Domain.Models.CarAds;
-    using Domain.Models.Dealers;
-
     using Microsoft.AspNetCore.Mvc;
 
     [ApiController]
     [Route("[controller]")]
     public class CarAdsController : ControllerBase
     {
-        private static readonly Dealer Dealer = new Dealer("Dealer", "+12345678");
-
         [HttpGet]
-        public IEnumerable<CarAd> Get() => Dealer.CarAds.Where(c => c.IsAvailable);
+        public IEnumerable<int> Get() => Enumerable.Range(1, 10).ToArray();
     }
 }
