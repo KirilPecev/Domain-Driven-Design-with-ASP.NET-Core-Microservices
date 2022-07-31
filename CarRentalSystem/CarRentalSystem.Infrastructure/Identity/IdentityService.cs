9bc882ab69def8af43da8db9c44e53a12aa1f5a0
Kiril Pecev <kir4o_pecev@abv.bg>
2022-07-31 20:14:36 +0300

    Added IdentityService in Infrastructure layer

---
+++ CarRentalSystem/CarRentalSystem.Infrastructure/Identity/IdentityService.cs
@@ -0,0 +1,82 @@
+﻿namespace CarRentalSystem.Infrastructure.Identity
+{
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
+
+    using Application;
+    using Application.Contracts;
+    using Application.Features.Identity;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Options;
+    using Microsoft.IdentityModel.Tokens;
+
+    public class IdentityService : IIdentity
+    {
+        private const string InvalidLoginErrorMessage = "Invalid credentials.";
+
+        private readonly UserManager<User> userManager;
+        private readonly ApplicationSettings applicationSettings;
+
+        public IdentityService(UserManager<User> userManager, IOptions<ApplicationSettings> applicationSettings)
+        {
+            this.userManager = userManager;
+            this.applicationSettings = applicationSettings.Value;
+        }
+
+        public async Task<Result<LoginOutputModel>> Login(UserInputModel userInput)
+        {
+            User user = await this.userManager.FindByEmailAsync(userInput.Email);
+            if (user == null)
+            {
+                return InvalidLoginErrorMessage;
+            }
+
+            bool isPasswordValid = await this.userManager.CheckPasswordAsync(user, userInput.Password);
+            if (!isPasswordValid)
+            {
+                return InvalidLoginErrorMessage;
+            }
+
+            string token = this.GenerateJwtToken(user.Id, user.Email);
+
+            return new LoginOutputModel(token);
+        }
+
+        public async Task<Result> Register(UserInputModel userInput)
+        {
+            User user = new User(userInput.Email);
+
+            IdentityResult identityResult = await this.userManager.CreateAsync(user, userInput.Password);
+
+            IEnumerable<string> errors = identityResult.Errors.Select(e => e.Description);
+
+            return identityResult.Succeeded ? Result.Success : Result.Failure(errors);
+        }
+
+        private string GenerateJwtToken(string userId, string email)
+        {
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(this.applicationSettings.Secret);
+
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, userId),
+                    new Claim(ClaimTypes.Name, email)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            string encryptedToken = tokenHandler.WriteToken(token);
+
+            return encryptedToken;
+        }
+    }
+}
