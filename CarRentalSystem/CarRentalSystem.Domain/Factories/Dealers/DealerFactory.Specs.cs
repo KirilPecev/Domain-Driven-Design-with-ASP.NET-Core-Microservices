f1db854e05398a3ea55c2de34c674de21b5f8d06
Kiril Pecev <kir4o_pecev@abv.bg>
2022-08-01 10:10:36 +0300

    Added factories -- Builder pattern

---
+++ CarRentalSystem/CarRentalSystem.Domain/Factories/Dealers/DealerFactory.Specs.cs
@@ -0,0 +1,24 @@
+﻿namespace CarRentalSystem.Domain.Factories.Dealers
+{
+    using FluentAssertions;
+    using Xunit;
+
+    public class DealerFactorySpecs
+    {
+        [Fact]
+        public void BuildShouldCreateDealer()
+        {
+            // Assert
+            var dealerFactory = new DealerFactory();
+
+            // Act
+            var dealer = dealerFactory
+                .WithName("TestDealer")
+                .WithPhoneNumber("+3591234567")
+                .Build();
+
+            // Assert
+            dealer.Should().NotBeNull();
+        }
+    }
+}
