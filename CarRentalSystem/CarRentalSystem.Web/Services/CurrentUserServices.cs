867ca1a3a1b4d419abd1b3ad9a0b9a2b8922e567
Kiril Pecev <kir4o_pecev@abv.bg>
2022-08-04 09:00:36 +0300

    Added ICurrentUser interface an application layer + implementation in web layer

---
+++ CarRentalSystem/CarRentalSystem.Web/Services/CurrentUserServices.cs
@@ -0,0 +1,26 @@
+﻿namespace CarRentalSystem.Web.Services
+{
+    using System.Security.Claims;
+
+    using Application.Contracts;
+
+    using Microsoft.AspNetCore.Http;
+
+    public class CurrentUserServices : ICurrentUser
+    {
+        public string UserId { get; }
+
+        public CurrentUserServices(IHttpContextAccessor httpContextAccessor)
+        {
+            ClaimsPrincipal? user = httpContextAccessor.HttpContext?.User;
+
+            if (user == null)
+            {
+                throw new InvalidOperationException(
+                    "This request does not have an authenticated user.");
+            }
+
+            this.UserId = user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
