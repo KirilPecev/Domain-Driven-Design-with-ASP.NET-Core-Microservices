8081b2745e7e8375a900751bb834d3c9e3b632a9
Kiril Pecev <kiril.pecev@ablera.com>
2022-10-04 16:11:16 +0300

    Admin ms implementation

---
+++ Microservices/CarRentalSystem.Admin/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,79 @@
+﻿namespace CarRentalSystem.Admin.Infrastructure
+{
+    using System;
+    using System.Reflection;
+
+    using CarRentalSystem.Admin.Services.Dealers;
+    using CarRentalSystem.Admin.Services.Identity;
+    using CarRentalSystem.Admin.Services.Statistics;
+    using CarRentalSystem.Infrastructure;
+    using CarRentalSystem.Services.Identity;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    using Refit;
+
+    using Services;
+
+    public static class ServiceCollectionExtensions
+    {
+        private static ServiceEndpoints serviceEndpoints;
+
+        public static IServiceCollection AddExternalService<TService>(
+            this IServiceCollection services,
+            IConfiguration configuration)
+            where TService : class
+        {
+            if (serviceEndpoints == null)
+            {
+                serviceEndpoints = configuration
+                    .GetSection(nameof(ServiceEndpoints))
+                    .Get<ServiceEndpoints>(config => config
+                        .BindNonPublicProperties = true);
+            }
+
+            string serviceName = typeof(TService)
+                .Name.Substring(1)
+                .Replace("Service", string.Empty);
+
+            services
+                .AddRefitClient<TService>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(serviceEndpoints[serviceName]));
+
+            return services;
+        }
+
+        public static void AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            ServiceEndpoints serviceEndpoints = configuration
+               .GetSection(nameof(ServiceEndpoints))
+               .Get<ServiceEndpoints>(config => config.BindNonPublicProperties = true);
+
+            services
+                .AddAutoMapperProfile(Assembly.GetExecutingAssembly())
+                .AddTokenAuthentication(configuration)
+                .AddHealth(
+                    configuration,
+                    databaseHealthChecks: false,
+                    messagingHealthChecks: false)
+                .AddScoped<ICurrentTokenService, CurrentTokenService>()
+                .AddTransient<JwtCookieAuthenticationMiddleware>()
+                .AddControllersWithViews(options => options
+                    .Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
+
+            services
+                .AddRefitClient<IIdentityService>()
+                .WithConfiguration(serviceEndpoints.Identity);
+
+            services
+                .AddRefitClient<IStatisticsService>()
+                .WithConfiguration(serviceEndpoints.Statistics);
+
+            services
+                .AddRefitClient<IDealersService>()
+                .WithConfiguration(serviceEndpoints.Dealers);
+        }
+    }
+}
--- Microservices/CarRentalSystem.Admin/Program.cs
+++ Microservices/CarRentalSystem.Admin/Program.cs
@@ -1,12 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using CarRentalSystem.Admin.Infrastructure;
+using CarRentalSystem.Infrastructure;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddServices(builder.Configuration);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -18,8 +25,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseJwtCookieAuthentication();
+
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseEndpoints(endpoints => endpoints
+        .MapHealthChecks()
+        .MapDefaultControllerRoute());
 
 app.Run();
